2ba8b16e5ef9bb05262c8acb755cc938649610a2
chad-tt0312 <chadwu.tt0312@gmail.com>
2026-06-03 17:55:46 +0800

    chore: remove build directory from git tracking

--- build/frontend/__tests__/setup-global.js
+++
@@ -1,8 +0,0 @@
-/**
- * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: CC0-1.0
- */
-
-export function setup() {
-	process.env.TZ = 'UTC'
-}
--- build/integration/features/bootstrap/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
-require __DIR__ . '/../../../../vendor-bin/behat/vendor/autoload.php';
-require __DIR__ . '/../../../../3rdparty/autoload.php';
